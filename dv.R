572e3e41e7d89724444c6c7da0f49c99c3fa38a9
Vince Buffalo <vsbuffaloAAAAAA@gmail.com>
2015-03-28 16:26:16 -0700

    bug fix

--- dv.R
+++ dv.R
@@ -18,7 +18,7 @@ quiet(library(docopt))
 "dv - command line devtools
 
 Usage:
-  dv create <pkg> [--check --no-rstudio]
+  dv create <path> [--check --no-rstudio]
   dv test [<pkg>]
   dv install [<pkg>]
   dv build [<pkg>]
@@ -39,47 +39,70 @@ Options:
   -D, --no-document   don't update and check documentation
 " -> doc
 
-#' Processing named arguments list, handling logicals e.g. turn
-#' --no-check-version into negated check_version
-process_args <- function(fun, args) {
-  # remove --[no-]?
-  fmls <- formals(fun)
-  #fmls <- fmls[sapply(fmls, is.logical)] # only handle logical
-  # formals 'form' with TRUE value have argumen --no-form, so look for that
-  sargs <- Map(function(n, v) {
-    if (!is.logical(v)) return(v) # don't process non-logicals here
-    argname <- if (v) paste0("--no-", n) else paste0("--", n)
-    # don't mess with any arguments not set
-    if (!(argname %in% names(args))) return(NULL)
-    arg_val <- args[[argname]] 
-    if (is.null(arg_val)) return(NULL)
-    val <- if (v) !arg_val else arg_val
-    val
-  }, names(fmls), fmls)
-  # drop ... from args
-  nodots <- sargs[setdiff(names(sargs), "...")]
-  nodots[!sapply(nodots, is.null)] # drop all nulls (revert to fun default)
+### May come back to this approach later; for now manually handing args is easy
+### enough
+###
+###
+###' Processing named arguments list, handling logicals e.g. turn
+###' --no-check-version into negated check_version
+##process_args <- function(fun, args) {
+##  # remove --[no-]?
+##  fmls <- formals(fun)
+##  #fmls <- fmls[sapply(fmls, is.logical)] # only handle logical
+##  # formals 'form' with TRUE value have argumen --no-form, so look for that
+##  sargs <- Map(function(n, v) {
+##    if (!is.logical(v)) {
+##      # don't process non-logicals here
+##      if (is.name(v)) # other special case: no default, NULL
+##        return(NULL)
+##      return(v)
+##    }
+##    argname <- if (v) paste0("--no-", n) else paste0("--", n)
+##    # don't mess with any arguments not set
+##    if (!(argname %in% names(args))) return(NULL)
+##    arg_val <- args[[argname]] 
+##    if (is.null(arg_val)) return(NULL)
+##    val <- if (v) !arg_val else arg_val
+##    val
+##  }, names(fmls), fmls)
+##  # drop ... from args
+##  nodots <- sargs[setdiff(names(sargs), "...")]
+##  nodots[!sapply(nodots, is.null)] # drop all nulls (revert to fun default)
+##}
+
+pkg_arg <- function(args) {
+  if (is.null(args$pkg)) return(".")
+  return(args$pkg)
 }
 
 build_call <- function(args) {
   quiet(library(devtools))
   # currently supported sub commands
-  dv_funs <- list(create=create,
-                  test=test,
-                  install=install,
-                  build=build,
-                  check=check,
-                  examples=run_examples,
-                  check_doc=check_doc)
+  dv_funs <- list(
+    create=function(args) create(args$path, check=args[['--check']],
+      rstudio=!args[['--no-rstudio']]),
+
+    test=function(args) test(pkg=pkg_arg(args)),
+
+    install=function(args) install(pkg=pkg_arg(args)),
+
+    build=function(args) build(pkg=pkg_arg(args)),
+
+    check=function(args) check(pkg=pkg_arg(args), cran=!args[['--no-cran']],
+      cleanup=!args[['--no-cleanup']], document=!args[['--no-document']]),
+
+    examples=function(args) run_examples(pkg=pkg_arg(args)),
+
+    check_doc=function(args) check_doc(pkg=pkg_arg(args)))
 
   fun_i <- which(unlist(args[names(dv_funs)]))
   fun <- dv_funs[[fun_i]]
-  do.call(fun, process_args(fun, args))
+  fun(args)
 }
 
 main <- function(args) {
   if (args[['--version']]) {
-    cat("version: %s\n\n", VERSION)
+    cat(sprintf("version: %s\n\n", VERSION))
   } else {
     build_call(args)
   }
